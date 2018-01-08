7caa2509e728b8ac0a5c5d576a93f0950d1926be
Mikey Ward <mw@mikey.io>
2018-01-08 11:29:32 -0500

    adds custom window (empty) to silence os bugs

---
+++ MonkeyDo/MonkeyDo/Editing Snippets/SnippetsWindow.swift
@@ -0,0 +1,15 @@
+//
+//  SnippetsWindow.swift
+//  MonkeyDo
+//
+//  Created by Michael L. Ward on 1/8/18.
+//  Copyright © 2018 Mikey Ward. All rights reserved.
+//
+
+import Cocoa
+
+class SnippetsWindow: NSWindow {
+
+}
+
+
